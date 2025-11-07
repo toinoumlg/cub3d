c1d99c8a74ad24334e9fcb7fe705fdd0f52b1147
toinoumlg <antoine.malanguerray@gmail.com>
2025-11-07 10:05:43 +0100

    starting some test with mlx and some strut

--- include/cub3d.h
+++ include/cub3d.h
@@ -1,18 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: amalangu <amalangu@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/11/07 07:57:59 by amalangu          #+#    #+#             */
-/*   Updated: 2025/11/07 09:58:47 by amalangu         ###   ########.fr       */
+/*   Created: 2025/11/07 10:01:37 by amalangu          #+#    #+#             */
+/*   Updated: 2025/11/07 10:03:35 by amalangu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <math.h>
+#ifndef CUB3D_H
+# define CUB3D_H
 
-int	main(void)
+typedef struct vector3
 {
-	
-}
+	float		x;
+	float		y;
+	float		z;
+}				t_vector3;
+
+typedef struct triangle
+{
+	t_vector3	v[3];
+};
+
+
+
+typedef struct cub3d
+{
+};
+
+#endif
