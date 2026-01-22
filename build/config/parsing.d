cf61aa8405ec6a39ea3cb242a9ff55d6f7fa701f
toinoumlg <antoine.malanguerray@gmail.com>
2026-01-22 18:41:14 +0100

    removed object

--- build/config/parsing.d
+++
@@ -1,7 +0,0 @@
-build/config/parsing.o: source/config/parsing.c include/config.h \
- include/struct.h mlx/mlx.h include/cub_utils.h include/logic.h
-include/config.h:
-include/struct.h:
-mlx/mlx.h:
-include/cub_utils.h:
-include/logic.h:
--- libft/build/ft_strtrim.d
+++
@@ -1,2 +0,0 @@
-build/ft_strtrim.o: source/ft_strtrim.c include/str.h
-include/str.h:
